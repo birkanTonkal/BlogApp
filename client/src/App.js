2e11379bd37c51e5b2c05d99d181f2a29f27981b
birkanTonkal <tonkalkamilbirkan@gmail.com>
2021-12-26 12:14:51 +0300

    router updated

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Navbar from "./components/navbar/Navbar";
 import SinglePost from "./pages/single-post/SinglePost";
 import Login from "./pages/login/Login";
 import Write from "./pages/write/Write";
+import Register from "./pages/register/Register";
+
 
 function App() {
     const user = true;
@@ -16,6 +18,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/post/:postId" element={<SinglePost />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/write" element={user ? <Write /> : <Login />} />
             </Routes>
         </Router>
