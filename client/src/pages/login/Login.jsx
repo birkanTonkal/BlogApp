ec3363f825790243a7e75df9f40eb1d8cb1318cf
birkanTonkal <tonkalkamilbirkan@gmail.com>
2021-12-28 16:48:43 +0300

    added errors for users

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 import { useRef } from "react";
 import axios from "axios";
 import { AuthContext } from "../../helpers/Context/AuthContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 function Login() {
     let username = useRef();
     let password = useRef();
-
+    const [error, setError] = useState();
     const { dispatch } = useContext(AuthContext);
     let submitHandler = async (e) => {
         e.preventDefault();
@@ -19,15 +19,30 @@ function Login() {
                 password: password.current.value,
             });
             dispatch({ type: "LOGIN_SUCCESS", payload: userData.data });
+            window.location.replace("/");
         } catch (e) {
-            dispatch({ type: "LOGIN_FAILURE", error: e.response.data });
+            if (
+                username.current.value === "" ||
+                password.current.value === ""
+            ) {
+                setError("I can't let you without information :(");
+                dispatch({ type: "LOGIN_FAILURE", error: e.response.data });
+            } else if (e.response.status === 400) {
+                setError(e.response.data);
+                dispatch({ type: "LOGIN_FAILURE", error: e.response.data });
+            } else {
+                dispatch({ type: "LOGIN_FAILURE", error: e.response.data });
+                setError("WE ARE IN MAINTENANCE")
+            }
         }
     };
 
     return (
         <div className="login-container">
             <div className="wrapper">
+                
                 <form className="login__form" onSubmit={submitHandler}>
+                    {error && <p className="error">&#9888;&nbsp;{error}</p>}
                     <label>Username</label>
                     <input
                         type="text"
@@ -40,13 +55,13 @@ function Login() {
                         placeholder="Please enter your password..."
                         ref={password}
                     />
-                    <button className="login__form-button">Login</button>
+                    <button className="login__form--button">Login</button>
+                    <Link className="link" to={"/register"}>
+                        <span className="register__link">
+                            Don't have account? Sign Up
+                        </span>
+                    </Link>
                 </form>
-                <Link className="link" to={"/register"}>
-                    <span className="register__link">
-                        Don't have account? Sign Up
-                    </span>
-                </Link>
             </div>
         </div>
     );
