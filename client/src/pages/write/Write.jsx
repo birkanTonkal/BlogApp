a2a705d8f7576260d3788339acffc34059785668
birkanTonkal <tonkalkamilbirkan@gmail.com>
2021-12-28 16:52:55 +0300

    wrapper ratio and font size changed

--- client/src/pages/write/Write.jsx
+++ client/src/pages/write/Write.jsx
@@ -32,43 +32,44 @@ function Write() {
         }
         try {
             const post = await axios.post("/post/write", newPost);
+            
             window.location.replace("/post/" + post.data._id);
         } catch (e) {
-            console.log("ERROR2" + e);
+            console.log(e);
         }
     };
     return (
         <div className="write-container">
             <div className="wrapper">
                 <form className="write__form" onSubmit={submitHandler}>
-                    <div className="write__form-img">
+                    <div className="write__form--img">
                         {file ? <img src={URL.createObjectURL(file)} alt="" />: <img src="http://localhost:5000/images/static_write.jpg" alt=""></img>}
                     </div>
-                    <div className="write__form-input">
+                    <div className="write__form--input">
                         <label htmlFor="imgInput">
-                            <i class="fas fa-folder-plus"></i>
+                            <i className="fas fa-folder-plus"></i>
                         </label>
                         <input
-                            className="write__form-input-file"
+                            className="write__form--input-file"
                             type="file"
                             id="imgInput"
                             onChange={(e) => setFile(e.target.files[0])}
                         />
                         <input
-                            className="write__form-input-title"
+                            className="write__form--input-title"
                             type="text"
                             placeholder="Write your title..."
                             autoFocus={true}
                             ref={title}
                         />
                     </div>
-                    <div className="write__form-text">
+                    <div className="write__form--text">
                         <textarea
-                            className="write__form-text-area"
+                            className="write__form--text-area"
                             placeholder="Write your blog..."
                             ref={content}
                         ></textarea>
-                        <button className="write__form-button" type="submit">
+                        <button className="write__form--button" type="submit">
                             Create Post
                         </button>
                     </div>
