4c48dafe8bd26eacfb475662cc89127d67ebda2f
birkanTonkal <tonkalkamilbirkan@gmail.com>
2021-12-28 16:47:52 +0300

    date added to comments

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -7,35 +7,35 @@ function Post({ post }) {
             <div className="post">
                 <div className="post__img">
                     <img
-                        src={"http://localhost:5000/images/"+ post.image}
+                        src={post.image !== "" ? "http://localhost:5000/images/" + post.image : "http://localhost:5000/images/defaulback.jpg"}
                         alt=""
                     />
                 </div>
                 <div className="post__content">
                     <Link className="link" to={`/?user=${post.username}`}>
-                        <div className="post__content-author">
+                        <div className="post__content--author">
                             <span>Author:{post.username}</span>
                         </div>
                     </Link>
                     <Link className="link" to={`/post/${post._id}`}>
-                        <div className="post__content-title">
+                        <div className="post__content--title">
                             <h3>{post.title}</h3>
                         </div>
                     </Link>
                     <Link className="link" to={`/post/${post._id}`}>
-                        <div className="post__content-text">
+                        <div className="post__content--text">
                             <p>{post.content}</p>
                         </div>
                     </Link>
 
-                    <div className="post__content-stats">
-                        <div className="post__content-stats-views">
+                    <div className="post__content--stats">
+                        <div className="post__content--stats-views">
                             <p>Views:{post.viewCount}</p>
-                            <p>0 comments</p>
+                            
                         </div>
-                        <div className="post__content-stats-likes">
-                            <p>31</p>
-                            <i className="fas fa-heart"></i>
+                        <div className="post__content--stats-comments">
+                        <p>Comments:{post.comments.length}</p>
+                            
                         </div>
                     </div>
                 </div>
